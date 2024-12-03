acccd209cfc1251c72c0f7f13582b980ffa8afe3
Reno <renogign@gmail.com>
2024-12-03 12:36:24 +0700

    Final

--- app/src/main/java/com/example/music_player_app/MyPlaylistProfile.java
+++ app/src/main/java/com/example/music_player_app/MyPlaylistProfile.java
@@ -3,13 +3,13 @@ package com.example.music_player_app;
 public class MyPlaylistProfile {
     public String MyPlaylistName;
     public String MyPlaylistDate;
-    public String img_url;
+    public String MyPlaylistImg;
 
     public Boolean terpilih = false;
 
     public MyPlaylistProfile(String pn, String pd, String pi) {
         this.MyPlaylistName = pn;
         this.MyPlaylistDate = pd;
-        this.img_url = pi;
+        this.MyPlaylistImg = pi;
     }
 }
